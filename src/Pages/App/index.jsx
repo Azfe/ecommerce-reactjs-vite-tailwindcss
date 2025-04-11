d4507017325043d5c44a92aa67f71261f78b8c1d
Alex Zapata <alexzapata1984@gmail.com>
2025-04-11 19:38:58 +0200

    Install React Router DOM + Add Routes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,23 +1,33 @@
-import './App.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import './App.css'
 
+const AppRoutes = () => {
 
-function App() {
+  const routes = useRoutes([
+    { path: '/', element: <Home />},
+    { path: '/my-account', element: <MyAccount />},
+    { path: '/my-order', element: <MyOrder />},
+    { path: '/my-orders', element: <MyOrders />},
+    { path: '/signin', element: <SignIn />},
+    { path: '/*', element: <NotFound />}
+  ])
+
+  return routes
+}
+
+const App = () => {
 
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+    
   )
 }
 
