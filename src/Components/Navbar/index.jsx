ddf0739ba73487e5c748f991a013ab2011732fb3
Alex Zapata <alexzapata1984@gmail.com>
2025-04-18 13:20:38 +0200

    Add Navbar

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,118 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    const activeStyle = 'underline underline-offset-4 decoration-2 decoration-primary font-semibold';
+
+    return (
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">           
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg">
+                    <NavLink
+                        to="/"
+                        end
+                    >
+                        Aether Skateshop
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/clothes"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/electronics"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/furnitures"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Furnitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/toys"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/others"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Others
+                    </NavLink>
+                </li>
+            </ul>
+            <ul className="flex items-center gap-3">                
+                <li>
+                    hello@aether.com
+                </li>
+                <li>
+                    <NavLink
+                        to="/my-orders"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/my-accounts"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/signin"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        Sig In
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/toys"
+                        className={({ isActive }) => (isActive ? activeStyle : undefined)}
+                        end
+                    >
+                        🛒 0
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
@@ -23,11 +24,11 @@ const AppRoutes = () => {
 
 const App = () => {
 
-  return (
+  return (    
     <BrowserRouter>
       <AppRoutes />
-    </BrowserRouter>
-    
+      <Navbar />
+    </BrowserRouter> 
   )
 }
 
